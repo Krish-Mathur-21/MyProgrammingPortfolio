d4a7de054cf947331b674f57c227709f6c6c95ad
Krish <techkrish216@gmail.com>
2025-11-20 18:31:11 +0530

    Final touches

--- calculator.cpp
+++ calculator.cpp
@@ -3,9 +3,28 @@ using namespace std;
 
 int main(){
 
-int numone, numtwo;
+double numone, numtwo;
 char op;
-
+cout << "Enter Two numbers : ";
 cin >> numone >> numtwo;
+cout << "Enter the operation you want to perform: ";
+cin >> op;
+switch(op)
+{
+case '+':
+cout << "sum : " << numone + numtwo;
+break;
+case '-':
+cout << "subtraction : " << numone - numtwo;
+break;
+case '*':
+cout << "Multiplication : " << numone * numtwo;
+case '/':
+cout << "Division : " << numone / numtwo;
+break;
+default:
+cout << "invalid operator" << endl;
+}
 
-switch
+return 0;
+}
